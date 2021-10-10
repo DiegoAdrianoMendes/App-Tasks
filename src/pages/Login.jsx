6379f3174e66109aef00755d85a5c67161d1ba04
DiegoAdrianoMendes <diegoadrianomendes@gmail.com>
2021-10-10 17:38:04 -0300

    Created a new Expo app

---
+++ src/pages/Login.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { 
+    StyleSheet, 
+    SafeAreaView,
+    View,
+} from 'react-native';
+
+export function Login(){
+    return( 
+        <SafeAreaView style={styles.container}>
+            <View>
+                
+            </View>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+})
---
+++ src/pages/Register.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { 
+    StyleSheet, 
+} from 'react-native';
+
+export function name(){
+    return( 
+
+    );
+}
+
+const styles = StyleSheet.create({
+    
+})
